ffbd2851c97c8349b58661ef20df466b722bfb09
JustCoDiEx <codiex1944@gmail.com>
2020-06-21 16:55:19 +0200

    Using inline assembly to call function from address with arguments.

--- InteropCpp.cpp
+++ InteropCpp.cpp
@@ -2,13 +2,60 @@
 //
 #include <iostream>
 #include <functional>
+#include <Windows.h>
 
+void test_no_args() {
+	std::cout << "Hello from test without args" << std::endl;
+}
 
+void __stdcall test_with_args(int v) {
+	std::cout << "Hello from test wth 1 arg: " << v << std::endl;
+}
+
+void __stdcall test_with_many_args(int a, int b) {
+	std::cout << "Hello from test wth 2 args: " << (a-b) << std::endl;
+}
+
+void runFunc() {
+
+	auto addr = &test_with_many_args;
+
+	std::cout << addr << std::endl;
+
+	__asm {
+
+		push 11
+		push 5
+		call [addr]
+
+	};
+
+}
+
+void runFunc(int argc, int* args) {
+
+	auto addr = &test_with_many_args;
+
+	for (size_t i = 0; i < argc; i++) {
+		int v = args[argc - 1 - i];
+		__asm {
+			push [v]
+		};
+	}
+
+	__asm {
+		call [addr]
+	};
+
+}
 
 int main() {
 
+	int args[2];
+	args[0] = 11;
+	args[1] = 5;
+	runFunc(2, args);
 
-
-
+	return 0;
 
 }
